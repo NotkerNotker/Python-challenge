90ba12b0e4228e29eed1150d7ec234abdf883ddb
Einhard <lukeellison17@yahoo.com>
2019-12-21 22:04:46 -0500

    more stuff

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,18 +12,20 @@ csvPath = "budget_data.csv"
 
 with open(csvPath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-
+    #take out header
     csv_header = next(csvfile)
     csvreader=list(csv.reader(csvfile))
     initial = (csvreader[0][1])
     for row in csvreader:
-        #print(row)
         months = months + 1
         net = net + int(row[1])
+        #calculate each difference in profits and add value to list
         change = int(row[1]) - int(initial)
         profits.append(change)
         initial = row[1]
-average = float(numpy.mean(profits))
+#calculate average of changes
+average = (numpy.mean(profits))
+
 print("")
 print("Financial Analysis")
 print("---------------------------------------------")
@@ -33,5 +35,14 @@ print("Average Change: "+"{:.2f}".format(average))
 print("Greatest Monthly Increase " +str(max(profits)))
 print("Greatest Monthly Decrease " +str(min(profits)))
 print("")
-# print(str(initial))
-# print(profits)
+
+with open("Output.txt", "w") as file:
+    file.write("\n")
+    file.write("Financial Analysis \n")
+    file.write("--------------------------------------------- \n")
+    file.write("Months: "+str(months)+"\n")
+    file.write("Total: "+str(net)+"\n")
+    file.write("Average Change: "+"{:.2f}".format(average)+"\n")
+    file.write("Greatest Monthly Increase " +str(max(profits))+"\n")
+    file.write("Greatest Monthly Decrease " +str(min(profits))+"\n")
+    file.write("\n")
